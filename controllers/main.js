f37ff683cf272c274f32edeaba46d9221133caa5
Eric Stefano Silva Milhomem <94946025+Ericssm96@users.noreply.github.com>
2022-09-21 14:58:30 -0300

    Main functionality implemented (application now verifies token)

--- controllers/main.js
+++ controllers/main.js
@@ -23,8 +23,16 @@ const dashboard = async (req, res) => {
     throw new CustomAPIError("Invalid credentials to access this route", 401);
   }
 
-  const luckyNumber = Math.floor(Math.random()*100);
-  res.status(200).json({msg: "Hello, John Doe", secret: `Since you're authorized, here is your lucky number: ${luckyNumber}.` })
+  const token = authHeader.split(' ')[1];
+
+  try{
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const {username} = decoded;
+    const luckyNumber = Math.floor(Math.random()*100);
+    res.status(200).json({msg: `Hello, ${username}`, secret: `Since you're authorized, here is your lucky number: ${luckyNumber}.` })
+  } catch (err) {
+    throw new CustomAPIError('Credentials not authorized to access this route', 401);
+  }
 }
 
 module.exports = {
