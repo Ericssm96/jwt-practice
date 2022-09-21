b4b7657f53bc9c81b0267f42803bd2f93a2fc071
Eric Stefano Silva Milhomem <94946025+Ericssm96@users.noreply.github.com>
2022-09-21 14:47:17 -0300

    Started setting up token

--- controllers/main.js
+++ controllers/main.js
@@ -1,15 +1,28 @@
+require("dotenv");
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
   const {username, password} = req.body;
   // console.log({username, password});
+
   if(!username || !password){
     throw new CustomAPIError('Please provide email and password', 400);
   }
-  res.send("Fake Login/Register/Signup Route");
+
+  const id = new Date().getDate();
+  const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+  res.status(200).json({msg: "user created", token});
 }
 
 const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader || !authHeader.startsWith("Bearer ")){
+    throw new CustomAPIError("Invalid credentials to access this route", 401);
+  }
+
   const luckyNumber = Math.floor(Math.random()*100);
   res.status(200).json({msg: "Hello, John Doe", secret: `Since you're authorized, here is your lucky number: ${luckyNumber}.` })
 }
